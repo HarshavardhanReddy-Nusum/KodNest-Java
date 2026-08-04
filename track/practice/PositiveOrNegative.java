d75ff55c7a3f707d4b1eb1d34cb210dbfa4ad0f2
HarshavardhanReddy-Nusum <harshavardhanreddynusum@gmail.com>
2026-08-04 12:24:21 +0530

    task 2

---
+++ track/practice/PositiveOrNegative.java
@@ -0,0 +1,14 @@
+
+public class PositiveOrNegative {
+
+    public static void main(String[] args) {
+        int num = -34;
+        if (num > 0) {
+            System.out.println("Positive");
+        } else if (num < 0) {
+            System.out.println("Negative");
+        } else {
+            System.out.println("Zero");
+        }
+    }
+}
