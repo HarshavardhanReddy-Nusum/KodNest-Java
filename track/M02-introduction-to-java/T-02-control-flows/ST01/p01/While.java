9e19cc99497f2a8e6043e7f155716dcaf15ac104
HarshavardhanReddy-Nusum <harshavardhanreddynusum@gmail.com>
2026-08-05 12:30:12 +0530

    while loop

---
+++ track/M02-introduction-to-java/T-02-control-flows/ST01/p01/While.java
@@ -0,0 +1,13 @@
+
+public class While {
+
+    public static void main(String[] args) {
+        int n = 1;
+        System.out.println("Before loop execution");
+        while (n <= 5) {
+            System.out.println(n);
+            n++;
+        }
+        System.out.println("After loop execution");
+    }
+}
