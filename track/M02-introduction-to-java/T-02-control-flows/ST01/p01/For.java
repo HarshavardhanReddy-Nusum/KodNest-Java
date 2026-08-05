245a1a7db496351fd700c3da943abcb3ac707557
HarshavardhanReddy-Nusum <harshavardhanreddynusum@gmail.com>
2026-08-05 12:20:04 +0530

    For loop

---
+++ track/M02-introduction-to-java/T-02-control-flows/ST01/p01/For.java
@@ -0,0 +1,13 @@
+
+public class For {
+
+    public static void main(String[] args) {
+        int n = 5;
+
+        System.out.println("Before loop execution");
+        for (int i = 1; i <= n; i++) {
+            System.out.println(i);
+        }
+        System.out.println("After loop execution ");
+    }
+}
