0ebf38fe18eecd8671023c596e45ce1b6fa28417
HarshavardhanReddy-Nusum <harshavardhanreddynusum@gmail.com>
2026-08-07 11:48:27 +0530

    sentence input from scanner

---
+++ track/M02-introduction-to-java/T-03-input-from-scanner/p01/Input2.java
@@ -0,0 +1,24 @@
+
+import java.util.Scanner;
+
+public class Input2 {
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Enter your age: ");
+        int age = sc.nextInt();
+        System.out.println("Your age is: " + age);
+
+        System.out.println("Enter your Height: ");
+        float height = sc.nextFloat();
+        System.out.println("Your height is: " + height);
+
+        sc.nextLine();
+
+        System.out.println("Enter your full name: ");
+        String fname = sc.nextLine();
+        System.out.println("Your name is: " + fname);
+    }
+}
