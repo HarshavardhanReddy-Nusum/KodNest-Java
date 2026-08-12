da7cafefddb5d83c017430aaf39f62ff458d0504
HarshavardhanReddy-Nusum <harshavardhanreddynusum@gmail.com>
2026-08-12 12:54:11 +0530

    method overloading

---
+++ track/M02-introduction-to-java/T-05-methods-variables-method_overloading/MethodOverloading.java
@@ -0,0 +1,41 @@
+
+public class MethodOverloading {
+
+    public static void main(String[] args) {
+        Addition a = new Addition();
+
+        System.out.println(a.add(10, 20));
+        System.out.println(a.add(10, 20, 30));
+        System.out.println(a.add(10.5, 20));
+        System.out.println(a.add(10.5, 20.5));
+        System.out.println(a.add(10.5, 20.5, 30.5));
+    }
+
+}
+
+class Addition {
+
+    int add(int a, int b) {
+        return a + b;
+    }
+
+    double add(int a, double b) {
+        return a + b;
+    }
+
+    int add(int a, int b, int c) {
+        return a + b + c;
+    }
+
+    double add(double a, int b) {
+        return a + b;
+    }
+
+    double add(double a, double b) {
+        return a + b;
+    }
+
+    double add(double a, double b, double c) {
+        return a + b + c;
+    }
+}
