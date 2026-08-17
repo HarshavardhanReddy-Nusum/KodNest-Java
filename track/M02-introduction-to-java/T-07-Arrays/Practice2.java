dc9cddf5ae1e15263153bc8cca59ad5736ef670a
HarshavardhanReddy-Nusum <harshavardhanreddynusum@gmail.com>
2026-08-17 13:23:44 +0530

    practice 2

--- track/M02-introduction-to-java/T-07-Arrays/Practice2.java
+++ track/M02-introduction-to-java/T-07-Arrays/Practice2.java
@@ -5,6 +5,18 @@ public class Practice2 {
         int[] a = {10, 20, 30, 40, 50};
         int bonus = 5;
 
+        int b[] = new int[5];
+        int j = 0;
+        for (int c : a) {
+            b[j] = c;
+            j++;
+        }
+
+        System.out.println("b: ");
+        for (int i = 0; i < b.length; i++) {
+            System.out.println(b[i]);
+        }
+
         for (int i = 0; i < a.length; i++) {
             a[i] += bonus;
         }
