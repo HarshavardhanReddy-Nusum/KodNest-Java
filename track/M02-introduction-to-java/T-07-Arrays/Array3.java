c69149f21245e476added048ff41a13296ab5917
HarshavardhanReddy-Nusum <harshavardhanreddynusum@gmail.com>
2026-08-17 12:51:41 +0530

    Arrays reverse printing

---
+++ track/M02-introduction-to-java/T-07-Arrays/Array3.java
@@ -0,0 +1,20 @@
+
+import java.util.Scanner;
+
+public class Array3 {
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        int[] a = new int[5];
+
+        for (int i = 0; i < a.length; i++) {
+            a[i] = sc.nextInt();
+        }
+
+        for (int i = a.length - 1; i >= 0; i--) {
+            System.out.println(a[i]);
+        }
+    }
+}
